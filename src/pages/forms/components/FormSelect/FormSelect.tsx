50ec8d6da2629c23f7296a31f1dd22efa79f7377
Andrey Svishchev <andrey.svishchev@yandex.ru>
2022-06-19 18:11:02 +0300

    add select

--- src/pages/forms/components/FormSelect/FormSelect.tsx
+++ src/pages/forms/components/FormSelect/FormSelect.tsx
@@ -17,47 +17,24 @@ const FormSelect: React.FC<PropsType> = (props) => {
         {value: 'chocolate', label: 'Chocolate'},
         {value: 'strawberry', label: 'Strawberry'},
         {value: 'vanilla', label: 'Vanilla'},
+        {value: 'chocolate', label: 'Chocolate'},
+        {value: 'strawberry', label: 'Strawberry'},
+        {value: 'vanilla', label: 'Vanilla'},
+        {value: 'chocolate', label: 'Chocolate'},
+        {value: 'strawberry', label: 'Strawberry'},
+        {value: 'vanilla', label: 'Vanilla'},
+        {value: 'chocolate', label: 'Chocolate'},
+        {value: 'strawberry', label: 'Strawberry'},
+        {value: 'vanilla', label: 'Vanilla'},
+        {value: 'chocolate', label: 'Chocolate'},
+        {value: 'strawberry', label: 'Strawberry'},
+        {value: 'vanilla', label: 'Vanilla'},
     ];
-
-    const customStyles = {
-        control: () => ({
-            border: '1px solid #DEDEDE',
-            borderRadius: '4px',
-            display: 'flex',
-            justifyContent: 'space-between',
-            width: '100%',
-        }),
-        valueContainer: () => ({
-            padding: '8px 16px',
-            display: 'flex',
-        }),
-        indicatorsContainer: () => ({
-            border: 'none',
-        }),
-        input: () => ({
-            margin: 0,
-            padding: 0
-        }),
-        placeholder: () => ({
-            color: '#818181'
-        }),
-        menu: (provided: any)=> ({
-      ...provided,
-            boxShadow: '0px 5px 10px rgba(0, 0, 0, 0.05)',
-
-            border: 'none',
-            borderRadius: '0px 0px 4px 4px',
-        }),
-
-
-
-    }
-
 
     return (
         <div className={s.select}>
             <div className={s.select__caption}>{caption}</div>
-            <Select options={options} styles={customStyles} placeholder={placeholder} />
+            <Select options={options} classNamePrefix='custom-select' placeholder={placeholder}/>
         </div>
     );
 };
